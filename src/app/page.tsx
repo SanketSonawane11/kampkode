01fe7f194b1606b88689e874f6d12abf66a1d7a4
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-08-14 20:20:13 +0530

    Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { OrbitingCirclesDemo } from '@/Components/orbiting-circles'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <OrbitingCirclesDemo />
+    </div>
+  )
+}
+
+export default page
